6b3f591ad589f7c2f339a12dda43d969263f25bc
Angel <angelquispe_2997@outlook.com>
2026-01-07 16:44:53 -0500

    creacion de resource postres, insumos, ventas, dashboard

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { PostresModule } from './postres/postres.module';
+import { InsumosModule } from './insumos/insumos.module';
+import { VentasModule } from './dashboard/ventas/ventas.module';
+import { VentasModule } from './ventas/ventas.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
   imports: [
@@ -18,6 +23,14 @@ import { AuthModule } from './auth/auth.module';
     }),
 
     AuthModule,
+
+    PostresModule,
+
+    InsumosModule,
+
+    VentasModule,
+
+    DashboardModule,
 
 
   ],
